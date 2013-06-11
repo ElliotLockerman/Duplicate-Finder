d30bd17070799df5f94cdcfc2216b7984615fd94
Elliot Lockerman <elliot.lockerman.info>
2013-06-11 17:21:09 -0400

    Separated classes into files. Fully functional.

--- duplicate finder.py
+++ duplicate finder.py
@@ -13,81 +13,12 @@ import os
 import subprocess
 import sys
 
+from duplicate_dictionary import *
+
 ###################################################################################
 # Classes
 
-class DuplicateDictionary():
-    
-    def __init__(self):
-        self.all_files = {} # A dictionary of all files in folder_to_search, including subfiles.Key is filename, value is path. 
-        self.duplicate_files = {}# A dictionary of all duplicate in folder_to_search, including subfiles. Key is filename, value is string of paths where the file exists.
-        self.ignore_list = []
-        pass
-    
-    def get_duplicate_files(self):
-        return self.duplicate_files
-    
-    def create(self, folder_to_search, ignore_items, gui):
-        # Show Progrgress window
-        self.progress_window = Toplevel()
-        self.progress_window.title("Duplicate Finder")
-        self.progress_window.resizable(FALSE,FALSE)
-        
-        self.progress_frame = ttk.Frame(self.progress_window, padding="10 10 10 10")
-        self.progress_frame.grid(column=0, row=0, sticky=(W, N, E, S))
-        
-        ttk.Label(self.progress_frame, text="Searching for duplicate files.").grid(column=1, row=1, padx="15", pady="15", sticky=E)
-        
-        self.progress_bar = ttk.Progressbar(self.progress_frame, orient=HORIZONTAL, length=200, mode='indeterminate')
-        self.progress_bar.grid(column=1, row=2, columnspan=3)
-        self.progress_bar.start()
-        
-        
-        self.folder_to_search_str = folder_to_search.get() # Get the toplevel directory
-        
-        
-        # Check if file exists. If not, make alert
-        if not os.path.exists(self.folder_to_search_str):
-            alert_window = Toplevel()
-            alert_window.title("Duplicate Finder")
-            alert_window.resizable(FALSE,FALSE)
-        
-            alertframe = ttk.Frame(alert_window, padding="10 10 10 10")
-            alertframe.grid(column=0, row=0, sticky=(W, N, E, S))
-        
-            ttk.Label(alertframe, text="The path you specified does not exist. Please try again").grid(column=0, row=1, padx="15", pady="15", sticky=E)
-            ttk.Button(alertframe, text="Ok", command=lambda: alert_window.destroy()).grid(column=0, row=2)
-            return
-         
-         
-        ignore_list = list(ignore_items.get().split(",")) # Get the ignore files, split by the commas, turn to list
-      
-        
-        # Main loop- walk through directories in folder_to_search 
-        for self.dirpath, self.dirnames, self.filenames in os.walk(self.folder_to_search_str):
-            
-            # Remove items on the ignore list
-            self.filenames = [x for x in self.filenames if x not in ignore_list]
-        
-            # Check what the file is and what to do with it, then do it
-            for file in self.filenames:
-                if file in self.all_files: #If its a duplicate,
-                    if file in self.duplicate_files: #And its already known to be a dupilcate
-                        self.duplicate_files[file].append(dirpath) # Add its path to duplicates list
-                    else: # And its not known to be a duplicate
-                        self.duplicate_files[file] = [self.all_files[file],]# Add the original (in all_files), creating a list
-                        self.duplicate_files[file].append(self.dirpath)# And add the current one
-                        
-                else: # If its not a duplicate
-                    self.all_files[file] = self.dirpath # If its not a duplicate, add to all files
-        
-                    
-        #Destory progress window            
-        self.progress_window.destroy()
-        
-        gui.ready()
-        
-        
+
 
 class GUI():
     
@@ -159,7 +90,7 @@ class GUI():
         self.file_listbox.grid(column=0, row=8, sticky=(W, N, E, S), padx=15)
         self.file_listbox.configure(bg='#ccc') # Starts out greyed out
         
-        self.file_listbox.bind("<<ListboxSelect>>", lambda x: gui.update_directory_listbox())
+        self.file_listbox.bind("<<ListboxSelect>>", lambda x: self.update_directory_listbox())
         
         
         
@@ -169,7 +100,7 @@ class GUI():
         self.directory_listbox.grid(column=1, row=8, columnspan=3, sticky=(W, N, E, S), padx=15)
         self.directory_listbox.configure(bg='#ccc') # Starts out greyed out
         
-        self.directory_listbox.bind("<<ListboxSelect>>", lambda x: gui.open_selected_path())
+        self.directory_listbox.bind("<<ListboxSelect>>", lambda x: self.open_selected_path())
                     
         self.root.mainloop()
         
@@ -219,7 +150,9 @@ class GUI():
         
         self.directory_listbox.delete(0, END) # Clear the box from previous selections
         
-        self.current_file_listbox_key = file_listbox.get(file_listbox.curselection())                
+        self.current_file_listbox_key = self.file_listbox.get(self.file_listbox.curselection())                
+        duplicate_files = duplicatedictionary.get_duplicate_files()
+        
         
         for directory in duplicate_files[self.current_file_listbox_key]:
             self.directory_listbox.insert(END, directory)
@@ -229,7 +162,7 @@ class GUI():
     # A function to open folders when clicked on and its bindings
     def open_selected_path(self):
         
-        path = directory_listbox.get(directory_listbox.curselection())
+        path = self.directory_listbox.get(self.directory_listbox.curselection())
        
         if sys.platform == 'darwin':
                 subprocess.check_call(['open', '--', path])
---
+++ duplicate_dictionary.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python
+
+###################################################################################
+# Imports
+from Tkinter import *
+import ttk
+from ttk import *
+import tkFileDialog
+
+
+import os
+
+import subprocess
+import sys
+
+###################################################################################
+
+
+
+class DuplicateDictionary():
+    
+    def __init__(self):
+        self.all_files = {} # A dictionary of all files in folder_to_search, including subfiles.Key is filename, value is path. 
+        self.duplicate_files = {}# A dictionary of all duplicate in folder_to_search, including subfiles. Key is filename, value is string of paths where the file exists.
+        self.ignore_list = []
+        pass
+    
+    def get_duplicate_files(self):
+        return self.duplicate_files
+    
+    def create(self, folder_to_search, ignore_items, gui):
+        # Show Progrgress window
+        self.progress_window = Toplevel()
+        self.progress_window.title("Duplicate Finder")
+        self.progress_window.resizable(FALSE,FALSE)
+        
+        self.progress_frame = ttk.Frame(self.progress_window, padding="10 10 10 10")
+        self.progress_frame.grid(column=0, row=0, sticky=(W, N, E, S))
+        
+        ttk.Label(self.progress_frame, text="Searching for duplicate files.").grid(column=1, row=1, padx="15", pady="15", sticky=E)
+        
+        self.progress_bar = ttk.Progressbar(self.progress_frame, orient=HORIZONTAL, length=200, mode='indeterminate')
+        self.progress_bar.grid(column=1, row=2, columnspan=3)
+        self.progress_bar.start()
+        
+        
+        self.folder_to_search_str = folder_to_search.get() # Get the toplevel directory
+        
+        
+        # Check if file exists. If not, make alert
+        if not os.path.exists(self.folder_to_search_str):
+            alert_window = Toplevel()
+            alert_window.title("Duplicate Finder")
+            alert_window.resizable(FALSE,FALSE)
+        
+            alertframe = ttk.Frame(alert_window, padding="10 10 10 10")
+            alertframe.grid(column=0, row=0, sticky=(W, N, E, S))
+        
+            ttk.Label(alertframe, text="The path you specified does not exist. Please try again").grid(column=0, row=1, padx="15", pady="15", sticky=E)
+            ttk.Button(alertframe, text="Ok", command=lambda: alert_window.destroy()).grid(column=0, row=2)
+            return
+         
+         
+        ignore_list = list(ignore_items.get().split(",")) # Get the ignore files, split by the commas, turn to list
+      
+        
+        # Main loop- walk through directories in folder_to_search 
+        for self.dirpath, self.dirnames, self.filenames in os.walk(self.folder_to_search_str):
+            
+            # Remove items on the ignore list
+            self.filenames = [x for x in self.filenames if x not in ignore_list]
+        
+            # Check what the file is and what to do with it, then do it
+            for file in self.filenames:
+                if file in self.all_files: #If its a duplicate,
+                    if file in self.duplicate_files: #And its already known to be a dupilcate
+                        self.duplicate_files[file].append(dirpath) # Add its path to duplicates list
+                    else: # And its not known to be a duplicate
+                        self.duplicate_files[file] = [self.all_files[file],]# Add the original (in all_files), creating a list
+                        self.duplicate_files[file].append(self.dirpath)# And add the current one
+                        
+                else: # If its not a duplicate
+                    self.all_files[file] = self.dirpath # If its not a duplicate, add to all files
+        
+                    
+        #Destory progress window            
+        self.progress_window.destroy()
+        
+        gui.ready()
+        
+        
