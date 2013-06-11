f1db0afc3f5e87b829482c778c045ca37eb597cd
Elliot Lockerman <elliot.lockerman.info>
2013-06-11 15:33:44 -0400

    File dictionary creation works.

--- duplicate finder.py
+++ duplicate finder.py
@@ -17,40 +17,39 @@ import sys
 
 class DuplicateDictionary():
     
-    # Set up variables for main loop
-    all_files = {} # A dictionary of all files in folder_to_search, including subfiles.Key is filename, value is path. 
-    duplicate_files = {}# A dictionary of all duplicate in folder_to_search, including subfiles. Key is filename, value is string of paths where the file exists.
-    ignore_list = []
 
 
-    def get_duplicate_files():
-        return duplicate_files
+    def get_duplicate_files(self):
+        return self.duplicate_files
     
     
-    __init__(self):
+    def __init__(self):
+        self.all_files = {} # A dictionary of all files in folder_to_search, including subfiles.Key is filename, value is path. 
+        self.duplicate_files = {}# A dictionary of all duplicate in folder_to_search, including subfiles. Key is filename, value is string of paths where the file exists.
+        self.ignore_list = []
         pass
         
     def create(self, folder_to_search, ignore_items):
         # Show Progrgress window
-        progress_window = Toplevel()
-        progress_window.title("Duplicate Finder")
-        progress_window.resizable(FALSE,FALSE)
+        self.progress_window = Toplevel()
+        self.progress_window.title("Duplicate Finder")
+        self.progress_window.resizable(FALSE,FALSE)
         
-        progress_frame = ttk.Frame(progress_window, padding="10 10 10 10")
-        progress_frame.grid(column=0, row=0, sticky=(W, N, E, S))
+        self.progress_frame = ttk.Frame(self.progress_window, padding="10 10 10 10")
+        self.progress_frame.grid(column=0, row=0, sticky=(W, N, E, S))
         
-        ttk.Label(progress_frame, text="Searching for duplicate files.").grid(column=1, row=1, padx="15", pady="15", sticky=E)
+        ttk.Label(self.progress_frame, text="Searching for duplicate files.").grid(column=1, row=1, padx="15", pady="15", sticky=E)
         
-        progress_bar = ttk.Progressbar(progress_frame, orient=HORIZONTAL, length=200, mode='indeterminate')
-        progress_bar.grid(column=1, row=2, columnspan=3)
-        progress_bar.start()
+        self.progress_bar = ttk.Progressbar(self.progress_frame, orient=HORIZONTAL, length=200, mode='indeterminate')
+        self.progress_bar.grid(column=1, row=2, columnspan=3)
+        self.progress_bar.start()
         
         
-        folder_to_search_str = folder_to_search.get() # Get the toplevel directory
+        self.folder_to_search_str = folder_to_search.get() # Get the toplevel directory
         
         
         # Check if file exists. If not, make alert
-        if not os.path.exists(folder_to_search_str):
+        if not os.path.exists(self.folder_to_search_str):
             alert_window = Toplevel()
             alert_window.title("Duplicate Finder")
             alert_window.resizable(FALSE,FALSE)
@@ -67,26 +66,26 @@ class DuplicateDictionary():
       
         
         # Main loop- walk through directories in folder_to_search 
-        for dirpath, dirnames, filenames in os.walk(folder_to_search_str):
+        for self.dirpath, self.dirnames, self.filenames in os.walk(self.folder_to_search_str):
             
             # Remove items on the ignore list
-            filenames = [x for x in filenames if x not in ignore_list]
+            self.filenames = [x for x in self.filenames if x not in ignore_list]
         
             # Check what the file is and what to do with it, then do it
-            for file in filenames:
-                if file in all_files: #If its a duplicate,
-                    if file in duplicate_files: #And its already known to be a dupilcate
-                        duplicate_files[file].append(dirpath) # Add its path to duplicates list
+            for file in self.filenames:
+                if file in self.all_files: #If its a duplicate,
+                    if file in self.duplicate_files: #And its already known to be a dupilcate
+                        self.duplicate_files[file].append(dirpath) # Add its path to duplicates list
                     else: # And its not known to be a duplicate
-                        duplicate_files[file] = [all_files[file],]# Add the original (in all_files), creating a list
-                        duplicate_files[file].append(dirpath)# And add the current one
+                        self.duplicate_files[file] = [self.all_files[file],]# Add the original (in all_files), creating a list
+                        self.duplicate_files[file].append(self.dirpath)# And add the current one
                         
                 else: # If its not a duplicate
-                    all_files[file] = dirpath # If its not a duplicate, add to all files
+                    self.all_files[file] = self.dirpath # If its not a duplicate, add to all files
         
                     
         #Destory progress window            
-        progress_window.destroy()
+        self.progress_window.destroy()
         
         displayduplicates.ready()
         
@@ -97,7 +96,7 @@ class DisplayDuplicates():
     def __init__(self):
         pass
         
-    def ready(): # Activates everything
+    def ready(self): # Activates everything
         # Display output
         #Change colors to show enabled
         file_listbox.configure(bg='#fff')
@@ -122,12 +121,12 @@ class DisplayDuplicates():
         
         # Populate file listbox
         file_listbox.delete(0, END) # Clear the box from previous searches
-        for file in duplicate_files.keys():
+        for file in duplicatedictionary.duplicate_files.keys():
             file_listbox.insert(END, file)
         file_listbox.selection_set(0)
         
         # Call to populate directory listbox
-        update_directory_listbox(duplicate_files, file_listbox)
+        self.update_directory_listbox(duplicate_files, file_listbox)
         
     
     # A function and binding to populate the directory listbox when file listbox is clicked
@@ -204,7 +203,6 @@ root.minsize(750,600)
 
 
 # Root-level frame
-mainframe
 mainframe = ttk.Frame(root, padding="30 30 30 30")
 mainframe.grid(column=0, row=0, sticky=(W, N, E, S))
 
